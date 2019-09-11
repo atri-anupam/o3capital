e9d00eaf1ef472810a17530596621b4d9a365ef5
Anupam Atri <anupamatri@Anupams-MacBook-Pro.local>
2019-09-11 11:59:48 +0530

    updating simulation from 5000 to 8000

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/getthrivescore', (req, res) => {
   var cashFlow = req.body.cashFlow;
   var flag = 0;
   var period = req.body.period
-  for(let runIndex =0; runIndex<5000; runIndex++) {
+  for(let runIndex =0; runIndex<8000; runIndex++) {
     var netWorthForOneSimulation = helpers.netWorthSimulation(cashFlow, period);
     if(netWorthForOneSimulation < 0) {
       flag = flag +1;
